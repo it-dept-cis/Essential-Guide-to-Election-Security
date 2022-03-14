b6043d73d0f275718f08c23302804a4671fb01d7
mike garcia <48967917+mike-garcia@users.noreply.github.com>
2022-03-14 14:29:22 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -301,7 +301,7 @@ latex_documents = [
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
 
-latex_logo = Media/'cis-logo.png'
+latex_logo = 'cis-logo.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
